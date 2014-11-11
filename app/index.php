5f0593da792aa83e9fd01f459aa58e75487e9bbf
fabienwang <fabienwang@eliteheberg.fr>
2014-11-11 20:23:02 +0100

    changing example app to short locale codes

--- app/index.php
+++ app/index.php
@@ -4,7 +4,7 @@ require_once('../weblighter/weblighter.php');
 
 echo (new \weblighter\weblighter(
  [
- 'en/home' => ['ctrl'=>'Controller_Home', 'func'=>'display', 'params'=>['lang'=>'en_US'] ],
- 'fr/home' => ['ctrl'=>'Controller_Home', 'func'=>'display', 'params'=>['lang'=>'fr_FR'] ],
+ 'en/home' => ['ctrl'=>'Controller_Home', 'func'=>'display', 'params'=>['lang'=>'en'] ],
+ 'fr/home' => ['ctrl'=>'Controller_Home', 'func'=>'display', 'params'=>['lang'=>'fr'] ],
  ]
 ))->display();
