45d8f82415bce356c70507c79eb8315154ddd4e4
fabienwang <fabienwang@eliteheberg.fr>
2014-12-16 21:40:06 +0100

    defining home action for demo app

--- app/index.php
+++ app/index.php
@@ -4,6 +4,7 @@ require_once('../weblighter/weblighter.php');
 
 echo (new \weblighter\weblighter(
  [
+ 'home' => 'Controller_Home',
  'en/home' => ['ctrl'=>'Controller_Home', 'func'=>'display', 'params'=>['lang'=>'en'] ],
  'fr/home' => ['ctrl'=>'Controller_Home', 'func'=>'display', 'params'=>['lang'=>'fr'] ],
  ]
